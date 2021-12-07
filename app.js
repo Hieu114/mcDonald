8a8238a2386f3179519982c547e93663c1ba5496
Hieu114 <biluclac0412@gmail.com>
2021-12-06 21:38:14 -0500

    Basic authentication
    Signed-off-by: Hieu114 <biluclac0412@gmail.com>

--- app.js
+++ app.js
@@ -3,16 +3,25 @@ const app = express();
 const path = require('path');
 const McDonald = require('./models/store');
 const mongoose = require('mongoose');
+const session = require('express-session');
+const flash = require('connect-flash');
 var methodOverride = require('method-override');
+const passport = require('passport');
 const ejsMate = require('ejs-mate');
 const Review = require('./models/review');
 const restaurant = require('./routes/stores');
+const LocalStrategy = require('passport-local');
+const User = require('./models/user');
 // const reviewRoutes = require('./routes/reviews');
+
+const userRoutes = require('./routes/user');
 
 mongoose.connect('mongodb://localhost:27017/mcDonald', {
     useNewUrlParser: true,
     useCreateIndex: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
 })
 
 const db = mongoose.connection;
@@ -23,13 +32,44 @@ db.once("open", ()=>{
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
-app.use('/stores', restaurant);
-
 
 app.set('views', path.join(__dirname, 'views'));
 app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')))
+
+const sessionConfig = {
+    secret: 'thisshouldbeabettersecret!',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7
+    }
+}
+
+app.use(session(sessionConfig))
+app.use(flash());
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
+app.use((req, res, next) => {
+    console.log(req.session)
+    res.locals.currentUser = req.user;
+    res.locals.success = req.flash('success');
+    res.locals.error = req.flash('error');
+    next();
+})
+
+app.use('/stores', restaurant);
+app.use('/', userRoutes);
 
 app.get('/', (req, res)=>{
     res.render('home.ejs',{who:'McDonald Home'})
@@ -57,8 +97,14 @@ app.post('/stores/:id/review', async (req, res)=>{
     res.redirect(`/stores/${s._id}`)
 })
 
-app.use((err,req,res,next)=>{
-    res.send("OH NO")
+app.all('*', (req, res, next) => {
+    next(new ExpressError('Page Not Found', 404))
+})
+
+app.use((err, req, res, next) => {
+    const { statusCode = 500 } = err;
+    if (!err.message) err.message = 'Something Went Wrong!'
+    res.status(statusCode).render('error', { err })
 })
 
 app.listen(3000, ()=> {
--- routes/stores.js
+++ routes/stores.js
@@ -1,17 +1,19 @@
 const express = require('express');
 const router = express.Router();
 const McDonald = require('../models/store')
+const catchAsync = require('../utils/catchAsync');
+const ExpressError = require('../utils/ExpressError');
+const { isLoggedIn } = require('../middleware');
 
-router.get('/', async (req, res)=>{
+router.get('/', catchAsync(async (req, res)=>{
     const stores = await McDonald.find({});
     res.render('stores.ejs', {stores, who:'All stores' })
-})
+}))
 
 router.post('/', async (req, res, next)=>{
     try{
         const s = new McDonald(req.body)
         await s.save()
-        console.log(s)
         res.redirect(`/stores/${s._id}`)
     }
     catch(e){
@@ -19,32 +21,32 @@ router.post('/', async (req, res, next)=>{
     }
 })
 
-router.get('/new', (req, res) =>{
+router.get('/new', isLoggedIn, (req, res) =>{
     res.render('new.ejs',{who: 'New Store'});
 })
 
-router.get('/:id', async (req, res) =>{
+router.get('/:id', catchAsync (async (req, res) =>{
     const {id} = req.params;
     const store = await McDonald.findById(id).populate('review');
     res.render('card.ejs', {store, id, who:'Info'})
-})
+}))
 
-router.put('/:id', async (req, res)=>{
+router.put('/:id', isLoggedIn, catchAsync (async (req, res)=>{
     const {id} = req.params;
     const s = await McDonald.findByIdAndUpdate(id, req.body, {runValidators: true, new: true});
     res.redirect(`/stores/${s._id}`)
-})
+}))
 
-router.get('/:id/edit', async (req, res)=>{
+router.get('/:id/edit', isLoggedIn, catchAsync (async (req, res)=>{
     const {id} = req.params;
     const s = await McDonald.findById(id);
     res.render('edit.ejs', {s, who: 'Edit store'});
-})
+}))
 
-router.delete('/:id', async (req, res)=>{
+router.delete('/:id', isLoggedIn, catchAsync (async (req, res)=>{
     const {id} = req.params;
     await McDonald.findByIdAndDelete(id);
     res.redirect('/stores')
-})
+}))
 
 module.exports = router;
